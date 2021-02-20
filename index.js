435b5f680e953ab7296469e69ccff2b2e5a0350f
Manish Makin <sunnymakin@gmail.com>
2021-02-20 15:42:28 -0600

    updates proxy with EC2 links

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const cors = require('cors')
 const PORT = 5000;
+
+// http://ec2-54-149-117-186.us-west-2.compute.amazonaws.com:5002/rooms/109/
+// http://ec2-54-188-70-61.us-west-2.compute.amazonaws.com:5008/
 
 app.use('/rooms/:id', express.static(__dirname + '/public'));
 
 app.get('/rooms/:id/getPhotosByRoomId', (req, res) => {
-  axios.get(`http://localhost:5005/rooms/${req.params.id}/getPhotosByRoomId`)
+  axios.get(`http://ec2-18-191-199-80.us-east-2.compute.amazonaws.com:5005/rooms/${req.params.id}/getPhotosByRoomId`)
   .then((photos) => {
     res.send(photos.data);
   })
@@ -17,7 +21,7 @@ app.get('/rooms/:id/getPhotosByRoomId', (req, res) => {
 });
 
 app.get('/users/:id', (req, res) => {
-  axios.get(`http://localhost:5007/users/${req.params.id}`)
+  axios.get(`http://ec2-34-210-111-179.us-west-2.compute.amazonaws.com:5007/users/${req.params.id}`)
   .then((users) => {
     res.send(users.data);
   })
@@ -28,7 +32,7 @@ app.get('/users/:id', (req, res) => {
 });
 
 app.get('/rooms/:id/availableDates', (req, res) => {
-  axios.get(`http://localhost:5001/rooms/${req.params.id}/availableDates`)
+  axios.get(`http://ec2-54-149-117-186.us-west-2.compute.amazonaws.com:5001/rooms/${req.params.id}/availableDates`)
   .then((dates) => {
     res.send(dates.data);
   })
@@ -39,7 +43,7 @@ app.get('/rooms/:id/availableDates', (req, res) => {
 });
 
 app.get('/rooms/:id/minNightlyRate', (req, res) => {
-  axios.get(`http://localhost:5001/rooms/${req.params.id}/minNightlyRate`)
+  axios.get(`http://ec2-54-149-117-186.us-west-2.compute.amazonaws.com:5001/rooms/${req.params.id}/minNightlyRate`)
   .then((rate) => {
     res.send(rate.data);
   })
